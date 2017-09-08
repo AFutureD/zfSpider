4165bd9d2b317d1a2f696a58a0184795034474ba
Andy Francis <f20140807@outlook.com>
2017-09-08 17:20:17 +0800

    change file struct.

---
+++ switch_mode.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+pub_file="json/base_info_pub.json"
+debug_file="json/base_info_debug.json"
+base_file="json/base_info.json"
+
+# if [ ! -e "$pub_file" ]
+# then
+# 	echo "Already change to publishing mode"
+# else 
+# 	mv "$base_file" "$debug_file"
+# 	mv "$pub_file" "$base_file"
+# 	echo "success to change to publishing mode"
+# 	ls
+# fi
+
+# if [ ! -e "$debug_file" ]
+# then
+# 	echo "Already change to debug mode"
+# else 
+# 	mv "$base_file" "$pub_file"
+# 	mv "$debug_file" "$base_file"
+# 	echo "succcess to change to debug mode"
+# 	ls
+# fi
+
+if [ ! -e "$pub_file" ] && [ -e "$debug_file" ];
+then 
+	echo "It's in publishing mode."
+	echo "Changing to debug mode."
+	mv "$base_file" "$pub_file"
+	mv "$debug_file" "$base_file"
+	echo "succcess to change to debug mode."
+	cat $base_file
+elif [ -e "$pub_file" ] && [ ! -e "$debug_file" ];
+then
+	echo "It's in debug mode."
+	echo "Changing to publishing mode."
+	mv "$base_file" "$debug_file"
+	mv "$pub_file" "$base_file"
+	echo "Success to change to publishing mode."
+	cat $base_file
+fi
